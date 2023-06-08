051749cf0fe51c233ee5c29d6f47baf108a3863d
ryu <munam4@naver.com>
2023-06-08 20:45:44 +0900

    230608 현선 - 1388(solved)

--- 류현선/류현선/1388.c
+++ 류현선/류현선/1388.c
@@ -13,24 +13,25 @@ int main(void) {
     int m;
     scanf("%d %d", &n, &m);
     
-    char arr[n][m];
+    char arr[51][51];
+    for (int i=0; i<n; i++) {
+        scanf("%s", arr[i]);
+    }
     
+    int visited[51][51];
     for (int i=0; i<n; i++) {
         for (int j=0; j<m; j++) {
-            scanf("%c", &arr[i][j]);
+            visited[i][j] = 0;
         }
     }
     
-    // 2차원 배열 동적 할당. (입력받은 n행, m열 만큼 배열 생성하기)
-    int **visited = malloc(sizeof(int *) * n);
-    for (int i=0; i<n; i++){
-        visited[i] = malloc(sizeof(int) * m);
-    }
+//    for (int i=0; i<n; i++) {
+//        for (int j=0; j<m; j++) {
+//            printf("%c\n", arr[i][j]);
+//        }
+//    }
     
     int result = 0;
-    // printf("%c\n", arr[0][1]);
-    // printf("%c\n", arr[0][0]);
-    // printf("%c\n", arr[0][1]);
     for (int i=0; i<n; i++) {
         for (int j=0; j<m; j++) {
             if (visited[i][j] == 0) {
